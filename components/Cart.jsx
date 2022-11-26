587d531d6ca31e31aa60553ce51e27d4d6ec4a65
hamzasahin <hmzshn_11@hotmail.com>
2022-11-26 18:24:50 +0300

    components created and added products to sanity

---
+++ components/Cart.jsx
@@ -0,0 +1,10 @@
+
+const Cart = () => {
+    return (
+        <>
+            Cart
+        </>
+    )
+}
+
+export default Cart;        
---
+++ components/Layout.jsx
@@ -0,0 +1,10 @@
+
+const Layout = () => {
+    return (
+        <>
+            Layout
+        </>
+    )
+}
+
+export default Layout; 
